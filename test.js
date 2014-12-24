84563b73361623bd2db3f8f395caebd7b6da5acd
Mike Atkins <apeherder@gmail.com>
2014-12-24 17:09:04 -0500

    Tests now are human readable

--- test.js
+++ test.js
@@ -7,6 +7,19 @@ process.env.AWS_SSL_DISABLED = true;
 
 var test = require('tape');
 var hubot_woo = require('./index');
+var async = require('async');
+
+function mock_responder() {
+  return function(callback) {
+    hubot_woo({
+      respond: function(command_regex, responder) {
+        responder({
+          send: callback.bind(null, null)
+        });
+      }
+    });
+  };
+}
 
 test('Command is woo', function(t) {
   t.plan(3);
@@ -21,46 +34,31 @@ test('Command is woo', function(t) {
 
 test('Call send with message for empty bucket', function(t) {
   var s3 = require('./s3');
-  t.plan(3);
-  s3.createBucket({ Bucket: bucket }, function(err) {
+  t.plan(2);
+  async.series({
+    create_bucket: s3.createBucket.bind(s3, { Bucket: bucket }),
+    woo: mock_responder(),
+    delete_bucket: s3.deleteBucket.bind(s3, { Bucket: bucket })
+  }, function(err, results) {
     t.error(err);
-    hubot_woo({
-      respond: function(command_regex, responder) {
-        responder({
-          send: function(message) {
-            t.equal(message, 'no woo pics :\'(');
-            s3.deleteBucket({ Bucket: bucket }, function(err) {
-              t.error(err);
-            });
-          }
-        });
-      }
-    });
+    t.equal(results.woo, 'no woo pics :\'(');
   });
 });
 
 test('Call send with url for something in the bucket', function(t) {
   var s3 = require('./s3');
-  t.plan(5);
-  s3.createBucket({ Bucket: bucket }, function(err) {
-    t.error(err);
-    s3.putObject({ Bucket: bucket, Key: 'lol' }, function(err) {
+  t.plan(2);
+  async.series(
+    {
+      create_bucket: s3.createBucket.bind(s3, { Bucket: bucket }),
+      put_object: s3.putObject.bind(s3, { Bucket: bucket, Key: 'lol' }),
+      woo: mock_responder(),
+      delete_object: s3.deleteObject.bind(s3, { Bucket: bucket, Key: 'lol' }),
+      delete_bucket: s3.deleteBucket.bind(s3, { Bucket: bucket })
+    }, 
+    function(err, results) {
       t.error(err);
-      hubot_woo({
-        respond: function(command_regex, responder) {
-          responder({
-            send: function(message) {
-              t.equal(message, 'https://s3.amazonaws.com/foo/lol');
-              s3.deleteObject({ Bucket: bucket, Key: 'lol' }, function(err) {
-                t.error(err);
-                s3.deleteBucket({ Bucket: bucket }, function(err) {
-                  t.error(err);
-                });
-              });
-            }
-          });
-        }
-      });
-    });
-  });
+      t.equal(results.woo, 'https://s3.amazonaws.com/foo/lol');
+    }
+  );
 });
