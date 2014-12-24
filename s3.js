96f754ff4422a8a89e73d6f64cb26825a7403cdc
Mike Atkins <apeherder@gmail.com>
2014-12-24 16:17:44 -0500

    Correct config

--- s3.js
+++ s3.js
@@ -4,7 +4,7 @@ module.exports = new aws.S3({
   endpoint: process.env.AWS_ENDPOINT,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  sslEnabled: false,
+  sslEnabled: !process.env.AWS_SSL_DISABLED,
   region: 'us-east-1',
   s3ForcePathStyle: process.env.AWS_FORCE_PATH_STYLE
 });
